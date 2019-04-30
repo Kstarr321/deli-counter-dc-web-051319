011edada48b2d7bcaecc472735be9f680aa1145b
Kolton Starr <starrkolton@gmail.com>
2019-04-30 15:49:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 def line(arr)
   if arr == [ ]
-    puts "The line is currently empty"
+    puts "The line is currently empty."
   else
     puts "The line is currently: "
     arr.each_index do |idx|
