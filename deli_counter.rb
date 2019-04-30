c8bb6f8b59a5364981ece83ca2df9d2db51f8025
Kolton Starr <starrkolton@gmail.com>
2019-04-30 15:23:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,10 @@ def line(arr)
   if arr == [ ]
     puts "The line is currently empty."
   else 
+    new_line = []
     arr.each_index do |idx|
-      puts "The line is currently #{idx+1}. #{arr[idx]}"
+      new_line << "#{idx+1}. #{arr[idx]}"
     end
   end
+  puts "The line is currently #{new_line}"
 end 
-    
