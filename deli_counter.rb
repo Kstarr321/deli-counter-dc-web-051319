a8113e408a88b90a4b11f32e13f1490381afcd02
Kolton Starr <starrkolton@gmail.com>
2019-04-30 15:28:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,6 @@ def line(arr)
   if arr == [ ]
     puts "The line is currently empty."
   else 
-  puts "The line is currently 1. #{arr[0]} 2. #{arr[1]} 3. #{arr[2]}"
+  puts "The line is currently: 1. #{arr[0]} 2. #{arr[1]} 3. #{arr[2]}"
   end 
 end 
