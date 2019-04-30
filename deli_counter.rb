f589f11448c5fd9019a9da27dace5a4db9489721
Kolton Starr <starrkolton@gmail.com>
2019-04-30 15:12:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,8 @@ def line(arr)
   if arr == [ ]
     puts "The line is currently empty."
   else 
-    arr.each_index do |person|
-      ""
+    arr.each_index do |idx|
+      "Hello #{arr[idx]}, you are number #{idx+1} in line."
     end
   end
 end 
